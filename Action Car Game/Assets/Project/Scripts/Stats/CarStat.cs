48d3da2dda403cb049e79d9e2614fdefd64e661c
Satyam Vishwakarma <mavericksatyam@gmail.com>
2022-04-22 21:41:08 +0530

    Gave Stats To Each Player Car

---
+++ Action Car Game/Assets/Project/Scripts/Stats/CarStat.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarStat : MonoBehaviour
+{
+  public string carName;
+	public float maxHp, maxSpeed, handling;
+}
